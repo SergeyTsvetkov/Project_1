2f269ad2ad70dfd8fcc8d9ed9965d33ba6ea45a3
sergeyts <foap@yandex.ru>
2021-07-12 18:14:17 +0300

    Reading of URL encoding

---
+++ charget_1.py
@@ -0,0 +1,5 @@
+import urllib.request # модуль для открыеия url адресов
+import chardet        # детектор кодировки символов
+
+data = urllib.request.urlopen('http://yahoo.com/') #запрос к сайту
+print(chardet.detect(data.read())['encoding']) # читаем и определяем key[encoding]
